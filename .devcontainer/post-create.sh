93fa61b8e0b249f521405b1cc9a3414bab60144b
Hiroki Takeda <hiroki@omoide.life>
2024-02-27 17:33:01 +0900

    chore: update dev container features

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env sh
 sudo apt-get update;
 
-# AWS SAM
-curl -o /tmp/aws-sam-cli-linux-x86_64.zip -L https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip;
-unzip /tmp/aws-sam-cli-linux-x86_64.zip -d /tmp/sam;
-sudo /tmp/sam/install;
-rm -rf /tmp/aws-sam-cli-linux-x86_64.zip /tmp/sam;
-
 # Cobra
 go install github.com/spf13/cobra-cli@v1.3.0;
 
